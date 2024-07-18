11f86ae9a801a4c2d59d75a2e51f00ed1c751530
Mark Gruzdov <grumark198@gmail.com>
2024-07-18 16:48:14 +0300

    add ex.4 and ex.5

--- src/index.js
+++ src/index.js
@@ -3,8 +3,21 @@ import fastify from 'fastify';
 const app = fastify();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/users', (req, res) => {
+  res.send('GET /users');
+});
+
+app.post('/users', (req, res) => {
+  res.send('POST /users');
+});
+
+app.get('/hello', (req, res) => {
+  const name = req.query.name;
+  if (!name) {
+    res.send('Hello, World!');
+  } else {
+    res.send(`Hello, ${name}!`);
+  }
 });
 
 app.listen({ port }, () => {
